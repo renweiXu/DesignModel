a979d6c8da6b58221b5277d63a68fd8ccf1c1b1a
renweiXu <158167134@qq.com>
2019-03-16 16:34:33 +0800

    通过反射 创建对象实例

--- src/main/java/com/xu/factory/method/AnimalFactory.java
+++ src/main/java/com/xu/factory/method/AnimalFactory.java
@@ -21,10 +21,29 @@ public class AnimalFactory {
 			return null;
 		}
 	}
+	//利用反射
+	public <T extends Animal>T getInstance(Class<T> class1){
+		Animal animal = null;
+		try {
+			animal = (Animal)Class.forName(class1.getName()).newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (T) animal;
+	}
 	
 	public static void main(String[] args) {
 		AnimalFactory factory = new AnimalFactory();
 		Animal animal = factory.getAnimal("cat");
 		animal.eat();
+		Dog dog = factory.getInstance(Dog.class);
+		dog.eat();
 	}
 }
